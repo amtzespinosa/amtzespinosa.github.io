eb72167e04bc1822986f9eb56b4730d419ab454f
Alex M. Espinosa <76045216+amtzespinosa@users.noreply.github.com>
2023-07-23 19:06:43 +0200

    Add files via upload

--- projects/sheller/js/app.js
+++ projects/sheller/js/app.js
@@ -1,3 +1,91 @@
+const checkbox = document.getElementById("theme")
+
+checkbox.addEventListener("change", () => {
+
+    // body background
+
+    document.body.classList.toggle("body-darkmode");
+
+    // h1
+
+    var h1 = document.getElementsByTagName("h1");
+    h1[0].classList.toggle("h1-darkmode");
+
+    // h5 
+
+    var h5 = document.getElementsByTagName("h5");
+    h5[0].classList.toggle("h5-darkmode");
+    h5[1].classList.toggle("h5-darkmode");
+    h5[2].classList.toggle("h5-darkmode");
+
+    // Upper content div
+
+    var upperDiv = document.getElementById("upper-cont");
+    upperDiv.classList.toggle("upper-cont-darkmode");
+
+    // Info div
+
+    let infoDiv = document.getElementById("info-div");
+    infoDiv.classList.toggle("info-div-darkmode");
+
+    // Listener div
+
+    var listenerDiv = document.getElementById("listener-div");
+    listenerDiv.classList.toggle("listener-div-darkmode");
+
+    // IP and port 
+
+    var input = document.getElementsByTagName("input");
+    input[1].classList.toggle("input-darkmode");
+    input[2].classList.toggle("input-darkmode");
+
+    // Dropdown 
+
+    var select = document.getElementsByTagName("select");
+    select[0].classList.toggle("select-darkmode");
+
+    // Listener output
+
+    var listenerOut = document.getElementById("listener-output");
+    listenerOut.classList.toggle("listener-output-darkmode");
+
+    // Code outputs
+
+    var code = document.getElementsByTagName("code");
+    code[0].classList.toggle("code-darkmode");
+    code[1].classList.toggle("code-darkmode");
+
+    // Generate listener button
+
+    var generateBtn = document.getElementById("generate-btn");
+    generateBtn.classList.toggle("generate-btn-darkmode");
+
+    // Language selector div
+
+    var langSelector = document.getElementById("language-selector");
+    langSelector.classList.toggle("language-selector-darkmode");
+
+    // Language buttons
+
+    var selectBash = document.getElementById("select-bash");
+    selectBash.classList.toggle("select-bash-darkmode");
+
+    var selectPython = document.getElementById("select-python3");
+    selectPython.classList.toggle("select-python3-darkmode");
+
+    var selectPhp = document.getElementById("select-php");
+    selectPhp.classList.toggle("select-php-darkmode");
+
+    var selectPerl = document.getElementById("select-perl");
+    selectPerl.classList.toggle("select-perl-darkmode");
+
+    // Reverse Shell output code
+
+    var shellOut = document.getElementById("shell-output");
+    shellOut.classList.toggle("shell-output-darkmode");
+})
+
+
 function generateListener(){
     var ip = document.getElementById("ip").value;
     var port = document.getElementById("port").value;
